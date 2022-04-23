a17aad4ad16fd91219bb9ce003684e5f02d67ba8
Babby1216 <babbynigat5@gmail.com>
2022-04-23 11:20:02 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - Entry Point
+ *  Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int alp = 0;
+
+		while (alp < 26)
+		{
+			_putchar(alp + 97);
+			alp++;
+		}
+		i++;
+		_putchar('\n');
+	}
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - Entry Point
+ *  Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int alp = 0;
+
+		while (alp < 26)
+		{
+			_putchar(alp + 97);
+			alp++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}}
