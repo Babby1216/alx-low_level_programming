6310767e689ea9246c6e99a260030e126e816847
Babby1216 <babbynigat5@gmail.com>
2022-04-23 22:32:42 -0700

    s

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,89 +1,21 @@
 #include "main.h"
 /**
- * print_number - print out a number using printchar.
- * @n: the number you want to print using putchar
- * Return: 0.
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
  */
 void print_number(int n)
 {
-int  temp, temp2, cnt, div, result, i;
+	unsigned int x;
 
-cnt = 0;
-div = 1;
-if (n < 0)
-{
-_putchar('-');
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
-if (n == 0)
-{
-_putchar('0');
-}
-else
-{
-temp = n;
-while (temp)
-{
-temp /= 10;
-++cnt;
-}
-temp2 = cnt;
-while (temp2 > 1)
-{
-div *= 10;
---temp2;
-}
-for (i = 0; i < cnt; ++i)
-{
-result = n / div;
-if (result < 0)
-result *= -1;
-_putchar(result + '0');
-n %= div;
-div /= 10;
-}
-}
-#include "main.h"
-/**
- * print_number - print out a number using printchar.
- * @n: the number you want to print using putchar
- * Return: 0.
- */
-void print_number(int n)
-{
-int  temp, temp2, cnt, div, result, i;
-
-cnt = 0;
-div = 1;
-if (n < 0)
-{
-_putchar('-');
-}
-if (n == 0)
-{
-_putchar('0');
-}
-else
-{
-temp = n;
-while (temp)
-{
-temp /= 10;
-++cnt;
-}
-temp2 = cnt;
-while (temp2 > 1)
-{
-div *= 10;
---temp2;
-}
-for (i = 0; i < cnt; ++i)
-{
-result = n / div;
-if (result < 0)
-result *= -1;
-_putchar(result + '0');
-n %= div;
-div /= 10;
-}
-}
-}}
